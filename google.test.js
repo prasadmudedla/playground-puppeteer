f377acfe4f4ba7faa645327fbbb0c48d434794e5
prasadmudedla <prasadmudedla@live.in>
2019-10-24 13:21:45 +0530

    Fixing the Jest test

--- google.test.js
+++ google.test.js
@@ -2,7 +2,7 @@
 //Test using a jest-puppeteer
 describe('Google', () => {
     beforeAll(async () => {
-        wait page.setViewport({ width: 1680, height: 1050 });
+        await page.setViewport({ width: 1680, height: 1050 });
         await page.goto("https://www.google.com");
     });
 
